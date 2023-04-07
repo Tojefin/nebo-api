b5059ce8644e383c13aaf5393b67a6d3c1c5e8fc
Pavel <39239196+Tojefin@users.noreply.github.com>
2023-04-08 03:23:56 +0400

    Update server.js

--- server.js
+++ server.js
@@ -6,51 +6,52 @@ const express require('express')
 const cors require('cors')
 
 const app = express()
+const port = 80
+const key = process.env.key
+
 app.use(cors({
   origin: '*'
 }))
-const port = 80
 
-const key = process.env.key
+app.listen(port, () => {
+  console.log('Server start')
+})
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
 
-app.get('/api/store', async (req, res) => {
-// 	let data = await axios.get('https://easydonate.ru/api/v3/shop/products', {
-// 		headers: {
-// 			'Shop-key': key
-// 		}
-// 	}).then((res) => res.data)
+// app.get('/api/store', async (req, res) => {
+// // 	let data = await axios.get('https://easydonate.ru/api/v3/shop/products', {
+// // 		headers: {
+// // 			'Shop-key': key
+// // 		}
+// // 	}).then((res) => res.data)
 	
-// 	let editJson = []
-// 	data.response.forEach((item) => {
-// 		editJson.push({id: item.id, server_id: item.servers[0].id, name: item.name, price: item.price})
-// 	})
+// // 	let editJson = []
+// // 	data.response.forEach((item) => {
+// // 		editJson.push({id: item.id, server_id: item.servers[0].id, name: item.name, price: item.price})
+// // 	})
 	
-// 	console.log(editJson)
-//   res.json(editJson);
-	res.json([]);
-});
+// // 	console.log(editJson)
+// //   res.json(editJson);
+// 	res.json([]);
+// });
 
 // ?customer=Tojefin&server_id=50111&products={"389102":1}
-app.get('/api/payment', async (req, res) => {
-// 	let {customer, server_id, products} = req.query
+// app.get('/api/payment', async (req, res) => {
+// // 	let {customer, server_id, products} = req.query
 	
-// 	let data = await axios.get(`https://easydonate.ru/api/v3/shop/payment/create?customer=${customer}&server_id=${server_id}&products=${products}`, {
-// 		headers: {
-// 			'Shop-key': key
-// 		}
-// 	}).then((res) => res.data)
+// // 	let data = await axios.get(`https://easydonate.ru/api/v3/shop/payment/create?customer=${customer}&server_id=${server_id}&products=${products}`, {
+// // 		headers: {
+// // 			'Shop-key': key
+// // 		}
+// // 	}).then((res) => res.data)
 
-//   res.json(data.response.url);
-	res.json([]);
-});
+// //   res.json(data.response.url);
+// 	res.json([]);
+// });
 
-app.listen(port, () => {
-  console.log('Server start')
-})
 
 // export default app
 module.exports = app
