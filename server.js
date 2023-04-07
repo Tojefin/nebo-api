e464beb5dc68eca06cb7d9bc4b2a641217ff44c6
Pavel <39239196+Tojefin@users.noreply.github.com>
2023-04-08 03:33:36 +0400

    Update server.js

--- server.js
+++ server.js
@@ -55,5 +55,9 @@ app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 })
 
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
 // Export the Express API
 module.exports = app
