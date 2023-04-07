6a79ddfcc162193e4b324733c6a7b220df54384e
Pavel <39239196+Tojefin@users.noreply.github.com>
2023-04-08 02:41:44 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,19 @@
-var fetch require('node-fetch');
-var express require('express')
-var cors require('cors')
-var path require('path');
+const express = require('express')
 
 const app = express()
-app.use(cors({
-  origin: '*'
-}))
-const port = 80
+const PORT = 4000
 
-const key = process.env['key']
-
-app.get('/api/store', async (req, res) => {
-	let data = await fetch('https://easydonate.ru/api/v3/shop/products', {
-		headers: {
-			'Shop-key': key
-		}
-	})
-	let json = await data.json()
-	let editJson = []
-	json.response.forEach((item) => {
-		editJson.push({id: item.id, server_id: item.servers[0].id, name: item.name, price: item.price})
-	})
-	
-	console.log(editJson)
-  res.json(editJson);
-});
-
-// ?customer=Tojefin&server_id=50111&products={"389102":1}
-app.get('/api/payment', async (req, res) => {
-	let {customer, server_id, products} = req.query
-	let data = await fetch(`https://easydonate.ru/api/v3/shop/payment/create?customer=${customer}&server_id=${server_id}&products=${products}`, {
-		headers: {
-			'Shop-key': key
-		}
-	})
-	console.log(data)
-	let json = await data.json()
-  res.json(json.response.url);
-});
-
-app.listen(port, () => {
-  console.log('Server start')
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
 })
 
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
 module.exports = app
