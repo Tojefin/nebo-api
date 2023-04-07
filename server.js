8297a854da9f5c044cbf14bb05033cd86fb99a1f
Pavel <39239196+Tojefin@users.noreply.github.com>
2023-04-08 02:59:40 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
-const fetch require('node-fetch');
+const fetch = require('node-fetch');
+const cors = require('cors')
 
 const app = express()
 const PORT = 4000
+
+app.use(
+  cors({
+    origin: '*',
+  })
+)
 
 const key = process.env.key
 
