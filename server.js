9479629f049e486300a65457d934518f13f3c49a
Pavel <39239196+Tojefin@users.noreply.github.com>
2023-04-08 03:06:04 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const fetch = require('node-fetch');
+const fetch = require('unfetch');
 const cors = require('cors')
 
 const app = express()
@@ -22,34 +22,33 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/store', async (req, res) => {
-// 	let data = await fetch('https://easydonate.ru/api/v3/shop/products', {
-// 		headers: {
-// 			'Shop-key': key
-// 		}
-// 	})
-// 	let json = await data.json()
+	let data = await fetch('https://easydonate.ru/api/v3/shop/products', {
+		headers: {
+			'Shop-key': key
+		}
+	})
+	let json = await data.json()
 	let editJson = []
-// 	json.response.forEach((item) => {
-// 		editJson.push({id: item.id, server_id: item.servers[0].id, name: item.name, price: item.price})
-// 	})
+	json.response.forEach((item) => {
+		editJson.push({id: item.id, server_id: item.servers[0].id, name: item.name, price: item.price})
+	})
 	
-// 	console.log(editJson)
+	console.log(editJson)
   res.json(editJson);
 });
 
 // ?customer=Tojefin&server_id=50111&products={"389102":1}
 app.get('/api/payment', async (req, res) => {
-// 	let {customer, server_id, products} = req.query
-// 	let data = await fetch(`https://easydonate.ru/api/v3/shop/payment/create?customer=${customer}&server_id=${server_id}&products=${products}`, {
-// 		headers: {
-// 			'Shop-key': key
-// 		}
-// 	})
+	let {customer, server_id, products} = req.query
+	let data = await fetch(`https://easydonate.ru/api/v3/shop/payment/create?customer=${customer}&server_id=${server_id}&products=${products}`, {
+		headers: {
+			'Shop-key': key
+		}
+	})
 
-// 	let json = await data.json()
-	// 	console.log(data)
-//   res.json(json.response.url);
-	res.json([]);
+	let json = await data.json()
+		console.log(data)
+  res.json(json.response.url);
 });
 
 
