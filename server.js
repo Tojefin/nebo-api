f37ef5d50bbbc9876344af4459a4a076f023836c
Pavel <39239196+Tojefin@users.noreply.github.com>
2023-04-08 03:13:25 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,35 +1,25 @@
-const express = require('express')
-const fetch = require('unfetch');
-const cors = require('cors')
+import axios from 'axios';
+import express from 'express'
+import cors from 'cors'
+import path from 'path';
 
 const app = express()
-const PORT = 4000
-
-app.use(
-  cors({
-    origin: '*',
-  })
-)
+app.use(cors({
+  origin: '*'
+}))
+const port = 80
 
 const key = process.env.key
 
-app.listen(PORT, () => {
-  console.log(`API listening on PORT ${PORT} `)
-})
-
-app.get('/', (req, res) => {
-  res.send('Hey this is my API running 🥳')
-})
-
 app.get('/api/store', async (req, res) => {
-	let data = await fetch('https://easydonate.ru/api/v3/shop/products', {
+	let data = await axios.get('https://easydonate.ru/api/v3/shop/products', {
 		headers: {
 			'Shop-key': key
 		}
-	})
-	let json = await data.json()
+	}).then((res) => res.data)
+	
 	let editJson = []
-	json.response.forEach((item) => {
+	data.response.forEach((item) => {
 		editJson.push({id: item.id, server_id: item.servers[0].id, name: item.name, price: item.price})
 	})
 	
@@ -40,17 +30,18 @@ app.get('/api/store', async (req, res) => {
 // ?customer=Tojefin&server_id=50111&products={"389102":1}
 app.get('/api/payment', async (req, res) => {
 	let {customer, server_id, products} = req.query
-	let data = await fetch(`https://easydonate.ru/api/v3/shop/payment/create?customer=${customer}&server_id=${server_id}&products=${products}`, {
+	
+	let data = await axios.get(`https://easydonate.ru/api/v3/shop/payment/create?customer=${customer}&server_id=${server_id}&products=${products}`, {
 		headers: {
 			'Shop-key': key
 		}
-	})
+	}).then((res) => res.data)
 
-	let json = await data.json()
-		console.log(data)
-  res.json(json.response.url);
+  res.json(data.response.url);
 });
 
+app.listen(port, () => {
+  console.log('Server start')
+})
 
-
-module.exports = app
+export default app
