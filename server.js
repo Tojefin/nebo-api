a06e28026ec42c50b7b07891b75497922453d57a
Pavel <39239196+Tojefin@users.noreply.github.com>
2023-04-12 01:53:43 +0400

    Update server.js

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.get('/api/store', async (req, res) => {
 
 // ?customer=Tojefin&server_id=50111&products={"389102":1}
 app.get('/api/payment', async (req, res) => {
-	let {customer, server_id, products} = req.query
+	let {customer, server_id, products, coupon} = req.query
 	
-	let data = await axios.get(`https://easydonate.ru/api/v3/shop/payment/create?customer=${customer}&server_id=${server_id}&products=${products}`, {
+	let data = await axios.get(`https://easydonate.ru/api/v3/shop/payment/create?customer=${customer}&server_id=${server_id}&products=${products}&coupon=${coupon}`, {
 		headers: {
 			'Shop-key': key
 		}
